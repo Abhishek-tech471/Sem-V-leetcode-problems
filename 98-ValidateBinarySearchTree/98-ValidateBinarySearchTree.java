6d82953c40546e88f0191d4b61c0eabf99a69b66
Abhishek Yadav <149038672+Abhishek-tech471@users.noreply.github.com>
2025-08-08 21:35:41 +0530

    File updated at 8/8/2025, 9:35:41 PM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.java
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.java
@@ -1,4 +1,4 @@
-// Last updated: 8/8/2025, 9:30:24 PM
+// Last updated: 8/8/2025, 9:35:41 PM
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,29 +15,35 @@
  * }
  */
 class Solution {
-		public boolean isValidBST(TreeNode root) {
-			return ValidBST(root).isbst;
-		}
-
-		public BstPair ValidBST(TreeNode root) {
+		public TreeNode deleteNode(TreeNode root, int key) {
 			if (root == null) {
-				return new BstPair();
+				return null;
 			}
+			if (root.val < key) {
+				root.right = deleteNode(root.right, key);
+			} else if (root.val > key) {
+				root.left = deleteNode(root.left, key);
+			} else {
+				// 1 or 0 Child
+				if (root.left == null) {
+					return root.right;
+				} else if (root.right == null) {
+					return root.left;
+				} else {// both child
+					int max = max(root.left);
+					root.left=deleteNode(root.left, max);
+					root.val=max;
+				}
 
-			BstPair lbp = ValidBST(root.left);
-			BstPair rbp = ValidBST(root.right);
-			BstPair sbp = new BstPair();
-			sbp.min = Math.min(lbp.min, Math.min(rbp.min, root.val));
-			sbp.max = Math.max(lbp.max, Math.max(rbp.max, root.val));
-			sbp.isbst = lbp.isbst && rbp.isbst && lbp.max < root.val && rbp.min > root.val;
-			return sbp;
+			}
+			return root;
 
 		}
-
-		class BstPair {
-			boolean isbst = true;
-			Long max = Long.MIN_VALUE;
-			long min = Long.MAX_VALUE;
-
-		}
-	}
+        public int max(TreeNode root) {			
+            if (root == null) {				
+                return Integer.MIN_VALUE;			
+            }			
+            int max = max(root.right);			
+            return Math.max(max, root.val);		
+            }	
+}

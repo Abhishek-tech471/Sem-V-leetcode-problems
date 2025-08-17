9de6d836ccb51e54990461f4b7da38e72caf103f
Abhishek Yadav <149038672+Abhishek-tech471@users.noreply.github.com>
2025-08-18 00:30:03 +0530

    Create 460-LFUCache

---
+++ 460-LFUCache/460-LFUCache.java
@@ -0,0 +1,30 @@
+// Last updated: 8/18/2025, 12:30:03 AM
+class Solution {
+    public int minSteps(String s, String t) {
+        int [] arr1 = new int [26];
+        int arr2 [] = new int [26];
+        for(int i=0; i<s.length(); i++){
+            int a = s.charAt(i)-'a';
+            arr1[a]++;
+        }
+        for(int i=0; i<t.length(); i++){
+            int a = t.charAt(i)-'a';
+            arr2[a]++;
+        }
+
+        int pc=0;
+        int nc=0;
+
+        for(int i=0; i<26; i++){
+            if(arr1[i]>arr2[i]){
+                pc+=arr1[i]-arr2[i];
+            }
+            else{
+                nc+=arr2[i]-arr1[i];
+            }
+        }
+        System.out.println(pc);
+        System.out.println(nc);
+        return pc;
+    }
+}

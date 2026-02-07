1431c88d4feacad88e5f3e23e67dca5a943092ca
Abhishek Yadav <149038672+Abhishek-tech471@users.noreply.github.com>
2026-02-08 01:37:00 +0530

    File updated at 2/8/2026, 1:37:00 AM

--- 2744-FindMaximumNumberofStringPairs/2744-FindMaximumNumberofStringPairs.java
+++ 2744-FindMaximumNumberofStringPairs/2744-FindMaximumNumberofStringPairs.java
@@ -1,23 +1,17 @@
-// Last updated: 2/8/2026, 1:34:45 AM
-1class Solution {
-2    public int maximumNumberOfStringPairs(String[] words) {
-3        HashSet<String> isvisited = new HashSet<>();
-4        int c=0;
-5        for(int i =0; i<words.length;i++){
-6            String a =""+words[i].charAt(1)+words[i].charAt(0);
-7            System.out.println(a);
-8            if(isvisited.contains(a)){
-9                isvisited.remove(words[i]);
-10                c++;
-11            }
-12            // else if( ){
-13            //     c++;
-14            //     isvisited.remove((words[i].charAt(1)+words[i].charAt(0)) );
-15            // }
-16            else{
-17                isvisited.add(words[i]);
-18            }
-19        }
-20        return c;
-21    }
-22}
+// Last updated: 2/8/2026, 1:37:00 AM
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+        HashSet<String> hash= new HashSet<>();
+        int count =0;
+        for(String s : words){
+            String rev= new StringBuilder(s).reverse().toString();
+            if(hash.contains(rev)){
+                count ++;
+            }
+            else{
+                hash.add(s);
+            }
+        }
+        return count;
+    }
+}
